b575b7a67c63768db4f201629cdf25c7d18850ae
vivekVells <techengineervivek@gmail.com>
2018-03-15 12:12:43 -0400

    Log In page skeleton done

--- emptimelogmgmt/emptimeclklogmgmt/views.py
+++ emptimelogmgmt/emptimeclklogmgmt/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Welcome to Time Clock\'s Home page')
-    # render(request, 'emptimeclklogmgmt/index.html')
+    # return HttpResponse('Welcome to Time Clock\'s Home page')
+    return render(request, 'emptimeclklogmgmt/index.html', {})
